a23d67680a8910018da8b0c34edcc97b07fd2d63
Peter Johnstone <peter.wdj@gmail.com>
2017-12-11 16:57:40 +0000

    Minor changes

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "home page", :type => :request do
   it "Displays App Name" do
-    get '/'
+    get '/homepage'
     expect(page).to have_content("GRITINHO")
   end
 end
