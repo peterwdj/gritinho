07982d765c467b2ec806124d17f78b32ed50e994
Eustaquio122 <tiagofsousa@gmail.com>
2017-12-11 16:24:02 +0000

    Refactors stuff for Peter

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature HomepageController, type: :controller do
-  feature "User visits the home page" do
-    scenario "Displays Welcome message" do
-      visit '/'
-      expect(page).to have_content("GRITINHO")
-    end
+RSpec.describe "home page", :type => :request do
+  it "Displays App Name" do
+    get '/'
+    expect(page).to have_content("GRITINHO")
   end
 end
