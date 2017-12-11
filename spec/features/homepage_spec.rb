8181b2a7a9ad0b0218c2b357142f89f3abc0a4d7
Peter Johnstone <peter.wdj@gmail.com>
2017-12-11 17:16:39 +0000

    Update tests to reflect new root route

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,14 +2,19 @@ require "rails_helper"
 
 RSpec.feature "home page", :type => :feature do
   scenario "Displays App Name" do
-    visit "/homepage"
+    visit "/"
     expect(page).to have_content("GRITINHO")
   end
 
   scenario "Displays Nando's restaurant on front page" do
     Restaurant.create(name: "Nando's", description: "Cheeky banter with a side of chat")
-    visit '/homepage'
+    visit '/'
     expect(page).to have_content("Nando's")
     expect(page).to have_content("Cheeky banter with a side of chat")
+  end
+
+  scenario "Displays button allowing user to create new restaurant" do
+    visit "/"
+    expect(page).to have_button("New Restaurant")
   end
 end
