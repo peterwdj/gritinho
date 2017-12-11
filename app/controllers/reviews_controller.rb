f901808680a8e8eeff6d4a29bd509e299997cc1b
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-11 17:03:34 +0000

    Add review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
   def new
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to @review
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
 end
