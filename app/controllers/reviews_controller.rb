a1ebe4fcc4dabcb1274bdd415e14adb91a834320
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-11 17:12:42 +0000

    small styling change

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,8 @@ class ReviewsController < ApplicationController
   end
 
   private
-    def review_params
-      params.require(:review).permit(:rating, :comment)
-    end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
 end
