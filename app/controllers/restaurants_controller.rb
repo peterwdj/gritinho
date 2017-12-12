da50d174db5076bb06663da20c6fb88cbf78e060
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-12 15:21:50 +0000

    convert Restaurant routes, controllers and views to RESTful naming convention

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,9 @@
+class RestaurantsController < ApplicationController
+  def new	
+  end
+
+  def create
+    Restaurant.create(name: params[:name], description: params[:description])
+    redirect_to '/'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'restaurant/new'
 
   resources :reviews
   resources :restaurants
-
-  get '/homepage' => 'homepage#index'
-  get '/restaurant/new' => 'restaurant#new'
-
-  post '/restaurant/new' => 'restaurant#create'
 
   root 'homepage#index'
 end
