31157e54ef12aa32e42429679e54bf0619c77c18
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-12 17:15:49 +0000

    Add 1-to-many associations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,16 @@
 class RestaurantsController < ApplicationController
-  def new	
+  def new
   end
 
   def create
     Restaurant.create(name: params[:name], description: params[:description])
     redirect_to '/'
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = Review.find_by restaurant_id: 1
+  end
+
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,23 +14,4 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.default_formatter = 'doc' if config.files_to_run.one?
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
 end
