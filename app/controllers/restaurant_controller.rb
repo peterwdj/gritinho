d4599417fb07ae00485b007f1b49091fb5bf9376
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-13 14:57:43 +0000

    Connected reviews to restaurants

--- app/controllers/restaurant_controller.rb
+++
@@ -1,9 +0,0 @@
-class RestaurantController < ApplicationController
-  def new
-  end
-
-  def create
-	Restaurant.create(name: params[:name], description: params[:description])
-	redirect_to '/'
-  end
-end
