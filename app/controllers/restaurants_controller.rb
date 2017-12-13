d33a16757e1edc87ac3ed2bcafbbb907d9524b35
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-13 17:17:28 +0000

    adds update and delete method for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,8 +4,12 @@ class RestaurantsController < ApplicationController
   def new	
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def create
-    Restaurant.create(name: params[:name], description: params[:description])
+    Restaurant.create(restaurant_params_hash)
     redirect_to '/'
   end
 
@@ -14,5 +18,29 @@ class RestaurantsController < ApplicationController
     @reviews = Review.find_by restaurant_id: 1
   end
 
+  def destroy
+    Restaurant.find(params[:id]).destroy
+    redirect_to '/'
+  end
 
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def restaurant_params_hash
+    { name: params[:name], description: params[:description] }
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+  end
 end
+
