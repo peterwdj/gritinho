a1520bd6ee941c05049ac05ec598ee11b07b26ae
Peter Johnstone <peter.wdj@gmail.com>
2017-12-14 15:54:56 +0000

    Change User to Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'user/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :restaurants do
     resources :reviews
