84f82ff561a1de2ce03baf01ab32ffdb28649a4c
Peter Johnstone <peter.wdj@gmail.com>
2017-12-14 16:18:55 +0000

    Remove name from omniauth create user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     data = access_token.info
     user = User.where(email: data['email']).first
     unless user
-      user = User.create(name: data['name'], email: data['email'], password: Devise.friendly_token[0,20])
+      user = User.create(email: data['email'], password: Devise.friendly_token[0,20])
     end
     user
   end
